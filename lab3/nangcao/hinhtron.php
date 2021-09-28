e1288154d6af63126a2541d5b05e658a13c102f9
Nguyễn Hữu Nhật Trường - 60139138 <62973254+nhattruongcntt901@users.noreply.github.com>
2021-09-28 15:10:52 +0700

    hình tròn

---
+++ lab3/nangcao/hinhtron.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link
+        href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="http://localhost/baitap/css/style.css">
+    <link rel="stylesheet" href="http://localhost/css/font.css">
+    <title>TT SOCIAL</title>
+    <style>
+    .flex-doc {
+        display: -webkit-box;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        align-items: center;
+    }
+
+    .flex-ngang {
+        display: -webkit-box;
+        display: -moz-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        justify-content: center
+    }
+
+    img[src*="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"] {
+        display: none;
+    }
+    </style>
+</head>
+
+<body>
+    <?php
+    if(isset($_POST['bankinh']))
+    {
+        if($_POST['bankinh']!="" && $_POST['bankinh']>=0)
+        {
+            $bankinh = $_POST['bankinh'];
+            $dientich = $bankinh*$bankinh*3.14;
+            echo"
+            <div class='row w-100' style='position:fixed;margin-top:33vh'>
+                <div class='col-md-10'>
+        
+                </div>
+                <div class='col-md-2'>
+                    <div class='post font-lato-heavy animate__animated animate__bounceInDown' style='font-size: 1em;color:rgb(71, 69, 110)' id='checkloidk'>
+                        <b>Kết quả</b>
+                        <div class='row no-gutters flex-ngang'>
+                            <hr width='90%' color='white' style='opacity: 30%;' />
+                        </div>
+                        <div>
+                            <p>Bán kính: $bankinh</p>
+                            <p>Diện tích: $dientich</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            ";
+        }
+    }
+?>
+    <div class='flex-doc flex-ngang' style='height:100vh'>
+        <form class='post w-50 flex-ngang' action='hinhtron.php' method='POST'>
+            <div>
+                <h2 style='color:rgb(255,255,255,0.6)'>Tính diện tích hình tròn</h2>
+                <div class='form-outline'>
+                    <div style="margin-left:20px;" class="flex-doc">
+                        <span class="material-icons-outlined">straighten</span>
+                        <input class="form-in" type="number" name="bankinh" <?php if(isset($_POST['bankinh'])) echo "value='$bankinh'" ?>  placeholder="Nhập bán kinh" required />
+                    </div>
+                </div>
+                <button class="w-100 btn" type="submit"
+                    style="background-color: rgb(60, 59, 97);color:white">Tính</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
