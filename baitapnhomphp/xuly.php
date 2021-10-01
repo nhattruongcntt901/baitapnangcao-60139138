c53d11423621d8af776b26869dffadf0abdf9a8c
Nguyễn Hữu Nhật Trường - 60139138 <62973254+nhattruongcntt901@users.noreply.github.com>
2021-10-01 19:02:41 +0700

    update

---
+++ baitapnhomphp/xuly.php
@@ -0,0 +1,23 @@
+<?php
+    $id_m = $_GET['id_music'];
+
+
+    if($id_m=="huong")
+    {?>
+        <div style="background-color: rgb(65, 116, 225);height: 50px;width: 50px;border-radius: 50%;"></div>
+            <div class="gian-cach">
+                <span><b>Hương</b></span><br>
+                <span>Văn Mai Hương</span>
+            </div>
+    <?php
+    }
+    if($id_m=="Forgetmenow")
+    {?>
+            <div style="background-color: rgb(65, 116, 225);height: 50px;width: 50px;border-radius: 50%;"></div>
+            <div class="gian-cach">
+                <span><b>Forget Me Now</b></span><br>
+                <span>Nhật Trường</span>
+            </div>
+    <?php
+    }
+?>
